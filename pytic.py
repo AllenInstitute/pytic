037926001bc82e8bc53953175ce3e7bf9408a410
ALLENINST\danc <danc@alleninstitute.org>
2018-06-19 11:07:25 -0700

    change ordering accoding to cli.cpp, once verified, turn code to python like.

--- pytic.py
+++ pytic.py
@@ -3,6 +3,9 @@ PyTic - Python Interface for Pololu Tic Stepper Motor Controllers
 Author: Daniel Castelli
 Date: 6/18/2018
 Version: 0.0.1
+
+Status:
+- error structure needed to parse command errors [priority-B]
 '''
 
 import sys
@@ -34,6 +37,7 @@ print(ticlib.tic_settings_create(byref(settings_p)))
 settings = settings_p[0]
 # Default Settings - must set product first
 print(ticlib.tic_settings_set_product(byref(settings),c_uint8(t_const['TIC_PRODUCT_T825'])))
+print('here')
 print(ticlib.tic_settings_fill_with_defaults(byref(settings)))
 print(ticlib.tic_settings_set_ignore_err_line_high(byref(settings),c_bool(True)))
 print(ticlib.tic_settings_set_serial_crc_enabled(byref(settings),c_bool(False)))
@@ -63,17 +67,15 @@ print(ticlib.tic_reinitialize(byref(t_handle)))
 print("\nRunning Motion Commands...")
 print(ticlib.tic_exit_safe_start(byref(t_handle)))
 print(ticlib.tic_energize(byref(t_handle)))
-# print(ticlib.tic_set_max_speed(byref(t_handle), c_uint32(500000000)))
+# print(ticlib.tic_set_max_speed(byref(t_handle), c_uint32(120000000)))
 # print(ticlib.tic_set_max_speed(byref(t_handle), c_uint32(10000)))
 # print(ticlib.tic_set_max_accel(byref(t_handle), c_uint32(50000000)))
 print(ticlib.tic_halt_and_set_position(byref(t_handle), c_int32(0)))
 print(ticlib.tic_reinitialize(byref(t_handle)))
 #print(ticlib.tic_reset_command_timeout(byref(t_handle)))
-#print(ticlib.tic_set_target_position(byref(t_handle), c_int32(1650)))
-# print(ticlib.tic_set_target_position(byref(t_handle), c_int32(1597*6)))
-print(ticlib.tic_set_target_position(byref(t_handle), c_int32(1597)))
+
 print(ticlib.tic_set_target_position(byref(t_handle), c_int32(1597*2)))
-#print(ticlib.tic_set_target_velocity(byref(t_handle), c_int32(1000000)))
+#print(ticlib.tic_set_target_velocity(byref(t_handle), c_int32(120000000)))
 
 sleep(3)
 
@@ -107,11 +109,6 @@ for code in ecodes:
     if (e_bit_mask & t_const[code]) == t_const[code]:
         print("Error: " + code)
 
-
-# BIT MASK REQUIRED TO GET ERROR INFO FOR VARIABLE STATE
-# errors_p = pointer(variables.errors_occurred)
-# print(ticlib.tic_error_get_message(errors_p))
-
 # Close down motor
 print(ticlib.tic_clear_driver_error(byref(t_handle)))
 print(ticlib.tic_deenergize(byref(t_handle)))
@@ -124,117 +121,16 @@ for i in range(0,3):
     print(ticlib.tic_variables_get_digital_reading(byref(variables),t_const['TIC_PIN_NUM_RX']))
     sleep(1)
 
-#print(ticlib.tic_reset(byref(t_handle)))
-
-# INTERNAL DEVICE CASTING, USEFUL LATER
+# INTERNAL DEVICE CASTING, EXAMPLE #1
 # devchk = cast(handle_p[0].device, POINTER(tic_device))
 # print(devchk.contents.serial_number)
-# print(newtic.serial_number)
-# print(pp)
-# print(pp[0][0].serial_number)
-# print(ticlib.tic_deenergize(byref(h)))
-# print(ticlib.tic_set_target_velocity(byref(h),c_int(500)))
 
-# TRACE COMMANDS - DEBUG
-# import pdb; pdb.set_trace()
-
-# HACKED VERSION OF CODE, REQUIRES MODIFIED SOURCE
-# devcnt = c_size_t(0)
-# t = pointer(c_long())
-
-# ticlib.pytic_list_connected_devices.restype=tic_device
-# ticdev = ticlib.pytic_list_connected_devices(t, byref(devcnt))
-# print(ticdev)
-# print(ticdev.usb_interface)
-# print(ticdev.serial_number)
-# print(ticdev.os_id)
-# print(ticdev.firmware_version)
-# print(ticdev.product)
-
+# INTERNAL DEVICE CASTING, EXAMPLE #2
 # usbint = cast(ticdev.usb_interface, POINTER(libusbp_generic_interface))
 # print(usbint.contents.interface_number)
 # print(usbint.contents.device_instance_id)
 # print(usbint.contents.filename)
 
-# h_temp = pointer(c_long())
-
-# ticlib.pytic_handle_open.restype=tic_handle
-# h = ticlib.pytic_handle_open(byref(ticdev), h_temp)
-# print(h)
-# print(h.usb_handle)
-# print(h.device)
-
-# chk = cast(h.device, POINTER(tic_device))
-# print(chk.contents.os_id)
-
-
-
-
-
-
-
-
-
-
-
-# ticdev = tic_device()
-# tp = pointer(ticdev)
-# tpp = pointer(tp)
-# tppp = pointer(tpp)
-
-# t = pointer(c_long())
-
-# print(ticlib.tic_list_connected_devices(t, byref(devcnt)))
-# print(devcnt.value)
-
-# dev_array = cast(t, POINTER(tic_device*devcnt.value))
-# print(dev_array[0][0].serial_number)
-
-
-# print(ticdev.os_id)
-# print(ticlib.please_work_now())
-
+# DEBUG TRACE EXAMPLE
 # import pdb;pdb.set_trace()
 
-# print(tp.contents.product)
-
-# print(ticlib.tic_list_connected_devices(byref(tl), byref(devcnt)))
-# ticdev_p = POINTER(tic_device) 
-# tic_device_list = (ticdev_p) * 5
-# tdl = tic_device_list()
-# print(tl[0])
-# h = cast(tl[0],POINTER(tic_device))
-# print(h)
-# print(h.contents.serial_number)
-
-# tic_device_list = tic_device * 5
-# tdl = tic_device_list()
-# tic_list = c_ulonglong * 5
-# tl = tic_list()
-# t = c_ulonglong()
-
-# usbint = libusbp_generic_interface()
-# ticdev = tic_device()
-
-# for i in range(0,1):
-# print(t)
-# print(t.value)
-# ticdev
-# for i in range(0,5):
-#     print(tdl[i].serial_number)
-# print(ticdev.libusbp_generic_interface)
-# print(ticdev.firmware_version)
-# print(ticdev.product)
-# print(ticlib.tic_list_connected_devices(byref(ticdev), byref(devcnt)))
-# print(ticlib.tic_list_connected_devices(byref(tdl), byref(devcnt)))
-
-#print(ticlib.tic_settings_get_serial_baud_rate(byref(settings)))
-#print(ticlib.tic_settings_set_command_timeout(byref(settings),c_uint16(60000)))
-#print(settings.command_timeout)
-#print(ticlib.tic_settings_set_soft_error_response(byref(settings), c_uint8(TIC_RESPONSE_GO_TO_POSITION)))
-
-# print(ticlib.tic_settings_free(byref(settings)))
-# print(ticlib.tic_settings_set_product(byref(settings),c_uint8(TIC_PRODUCT_ID_T825)))
-# print(ticlib.tic_settings_fill_with_defaults(byref(settings)))
-# print(settings.product)
-# print(settings.serial_baud_rate)
