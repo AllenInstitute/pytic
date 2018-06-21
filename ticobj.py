e22b097d351524cac45e5802f88b4205a6a3b7f1
ALLENINST\danc <danc@alleninstitute.org>
2018-06-20 17:06:01 -0700

    added [T]ic [E]rror [D]ecoder wrapper. add logging next.

--- ticobj.py
+++ ticobj.py
@@ -1,15 +1,30 @@
 import sys
+import yaml
 from ctypes import *
 from time import sleep
 from pytic_protocol import tic_constant as t_const
 from pytic_structures import *
+from functools import wraps
 
 usblib = windll.LoadLibrary("C:\\Users\\danc\\dev\\libusbp\\build\\libusbp-1.dll")
 ticlib = windll.LoadLibrary("C:\\Users\\danc\\dev\\tic\\build\\libpololu-tic-1.dll")
 
+# [T]ic [E]rror [D]ecoder
+def TED(func):
+    @wraps(func)
+    def func_wrapper(*args, **kwargs):
+        e_p = func(*args, **kwargs)
+        if bool(e_p):
+            e = cast(e_p, POINTER(tic_error))
+            print(e.message)
+            return 1
+        else:
+            return 0
+    return func_wrapper
+
 class TicObj(object):
     def __init__(self):
-        self.error_p = POINTER(tic_error)()
+        self._settings_create()
         
     def list_connected_serial_numbers(self):
         devcnt = c_size_t(0)
@@ -23,7 +38,17 @@ class TicObj(object):
             for i in range(0, devcnt.value):
                 ticdev = dev_pp[0][i]
                 print("Tic Device #: {0:d}, Serial #: {1:s}".format(i, ticdev.serial_number))
+    
+    def load_yaml_config(self):
+        pass
+
+    @TED
+    def _settings_create(self):
+        settings_p = POINTER(tic_settings)()
+        e_p = ticlib.tic_settings_create(byref(settings_p))
+        self.settings = settings_p[0]
+        return e_p
 
 if __name__ == '__main__':
     tic = TicObj()
-    tic.list_connected_serial_numbers()
+
