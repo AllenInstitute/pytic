3750711631194bb6a334faec134e59ed57bf7ec5
ALLENINST\danc <danc@alleninstitute.org>
2018-06-25 14:52:48 -0700

    fully functional Python interface to Pololu Tic C-API

--- ticobj.py
+++ ticobj.py
@@ -2,49 +2,57 @@ import sys
 import yaml
 from ctypes import *
 from time import sleep
-from pytic_protocol import tic_constant as t_const
+from pytic_protocol import tic_constant as tc
 from pytic_structures import *
 from functools import wraps, partial
 import logging
 import numpy as np
 
-# File Locations
-yml_config_file = "config.yml"
-usblib = windll.LoadLibrary("C:\\Users\\danc\\dev\\libusbp\\build\\libusbp-1.dll")
-ticlib = windll.LoadLibrary("C:\\Users\\danc\\dev\\tic\\build\\libpololu-tic-1.dll")
+'''
+Notes:
+    - make pip install files and re-order project folders
+'''
 
-logger = logging.getLogger('pytic_object')
+# Driver Locations (x64)
+usblib = windll.LoadLibrary("drivers\\x64\\libusbp-1.dll")
+ticlib = windll.LoadLibrary("drivers\\x64\\libpololu-tic-1.dll")
+
+# - Logging - 
+logger = logging.getLogger('PyTic')
 logger.setLevel(logging.DEBUG)
-fh = logging.FileHandler('pytic_object.log')
-fh.setLevel(logging.DEBUG)
+formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+# Console Logging
 ch = logging.StreamHandler()
 ch.setLevel(logging.DEBUG)
-formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
-fh.setFormatter(formatter)
 ch.setFormatter(formatter)
-logger.addHandler(fh)
 logger.addHandler(ch)
+## File Logging
+#fh = logging.FileHandler('PyTic.log')
+#fh.setLevel(logging.DEBUG)
+#fh.setFormatter(formatter)
+#logger.addHandler(fh)
 
 # [T]ic [E]rror [D]ecoder
 def TED(func):
     @wraps(func)
     def func_wrapper(*args, **kwargs):
-        e_p = func(*args, **kwargs)
-        if bool(e_p):
-            e = cast(e_p, POINTER(tic_error))
-            logger.error(e.contents.message)
+        _e_p = func(*args, **kwargs)
+        if bool(_e_p):
+            _e = cast(_e_p, POINTER(tic_error))
+            _logger = logging.getLogger('PyTic')
+            _logger.error(_e.contents.message)
             return 1
         else:
             return 0
     return func_wrapper
 
 class PyTic(object):
-    def __init__(self):
-        '''
-        Fold settings obj and variables obj into this object
-        '''
+    def __init__(self, log_file=None):
+        self._logger = logging.getLogger('PyTic') 
         self.device = None
         self.handle = None
+        self.settings = None
+        self.variables = None
         self._commands = [('set_target_position', c_int32),
                           ('set_target_velocity', c_int32),
                           ('halt_and_set_position', c_int32),
@@ -81,7 +89,7 @@ class PyTic(object):
     @TED
     def _tic_command_with_value(self, cmd_name, c_type, value):
         if 'TIC' in str(value):
-            value = t_const[value]
+            value = tc[value]
         e_p = getattr(ticlib,'tic_'+ cmd_name)(byref(self.handle), c_type(value))
         return e_p
 
@@ -113,20 +121,23 @@ class PyTic(object):
             if str(serial_number) == str(self._dev_pp[0][i].serial_number):
                 self.device = self._dev_pp[0][i]
                 self._tic_handle_open()
+                self.variables = PyTic_Variables(self.handle)
+                self.settings = PyTic_Settings(self.handle, self.variables.product)
                 return 0
         if not self.device:
-            logger.error("Serial number device not found.")
+            self._logger.error("Serial number device not found.")
             return 1
 
 
-class PyTic_Variable(object):
+class PyTic_Variables(object):
     def __init__(self, device_handle):
+        self._logger = logging.getLogger('PyTic') 
         self._device_handle = device_handle
         self._tic_variables_p = POINTER(tic_variables)()
         self._tic_variables = tic_variables()
         
         self.pin_info = []
-        for i in range(0, t_const['TIC_CONTROL_PIN_COUNT']):
+        for i in range(0, tc['TIC_CONTROL_PIN_COUNT']):
             self.pin_info.append(type('pinfo_'+str(i), (object,), {})())
 
         self._convert_structure_to_readonly_properties()
@@ -137,11 +148,10 @@ class PyTic_Variable(object):
                 prop = property(fget=partial(self._get_tic_readonly_property, field[0]))
                 setattr(self.__class__, field[0], prop)
         
-        for i in range(0, t_const['TIC_CONTROL_PIN_COUNT']):
+        for i in range(0, tc['TIC_CONTROL_PIN_COUNT']):
             for field in pin_info._fields_:
                 prop = property(fget=partial(self._get_pin_readonly_property, field[0], i))
                 setattr(self.pin_info[i].__class__, field[0], prop)
-
 
     @TED
     def _update_tic_variables(self):
@@ -152,103 +162,162 @@ class PyTic_Variable(object):
 
     def _get_tic_readonly_property(self, field, obj):
         self._update_tic_variables()
-        return getattr(self._tic_variables, field)
+        value = getattr(self._tic_variables, field)
+        if field == "error_status" or field == "error_occurred":
+            self._convert_error_bitmask(value)
+        return value
 
     def _get_pin_readonly_property(self, field, pin_num, obj):
         self._update_tic_variables()
         return getattr(self._tic_variables.pin_info[pin_num], field)
 
+    def _convert_error_bitmask(self, e_bit_mask):
+        ecodes = ["TIC_ERROR_INTENTIONALLY_DEENERGIZED",
+                  "TIC_ERROR_MOTOR_DRIVER_ERROR",
+                  "TIC_ERROR_LOW_VIN",
+                  "TIC_ERROR_KILL_SWITCH",
+                  "TIC_ERROR_REQUIRED_INPUT_INVALID",
+                  "TIC_ERROR_SERIAL_ERROR",
+                  "TIC_ERROR_COMMAND_TIMEOUT",
+                  "TIC_ERROR_SAFE_START_VIOLATION",
+                  "TIC_ERROR_ERR_LINE_HIGH",
+                  "TIC_ERROR_SERIAL_FRAMING",
+                  "TIC_ERROR_SERIAL_RX_OVERRUN",
+                  "TIC_ERROR_SERIAL_FORMAT",
+                  "TIC_ERROR_SERIAL_CRC",
+                  "TIC_ERROR_ENCODER_SKIP"]
+        for code in ecodes:
+            if ((e_bit_mask >> tc[code]) & 1):
+                self._logger.error(code)
+
         
 class PyTic_Settings(object):
-    def __init__(self, config_file):
-        '''
-        Convert to improved load format used in tic_variables and TIC macro recognition
-        for PyTic obj to make attributes part of host class and set in similar method using the structure class
-        '''
-        self.settings = tic_settings()
-        self._config_file = config_file
-        self._settings_create()
-        self._fill_with_defaults()
-        self._load_config_settings()
-        # below needs to be called for settings to take effect
-        # self.apply_settings_to_device() 
+    def __init__(self, device_handle, product):
+        self._logger = logging.getLogger('PyTic')
+        self._device_handle = device_handle
+        # local vs device - local settings on pc, device settings on tic
+        self._local_settings = tic_settings()
+        self._device_settings = tic_settings()
+        self._device_settings_p = POINTER(tic_settings)()
+        
+        self.pin_settings = []
+        for i in range(0, tc['TIC_CONTROL_PIN_COUNT']):
+            self.pin_settings.append(type('pset_'+str(i), (object,), {})())
+        
+        self._convert_structure_to_properties()
+        self.auto_apply = False
 
-    def _fill_with_defaults(self):
-        with open(self._config_file, 'r') as ymlfile:
-            cfg = yaml.load(ymlfile)
-        self.settings.product = t_const[cfg['settings']['product']]
-        ticlib.tic_settings_fill_with_defaults(byref(self.settings))
+        if "TIC" in str(product):
+            product = int(tc[product])
+        self._fill_with_defaults(product)
 
-    def apply_settings_to_device(self, device_handle):
+    def _convert_structure_to_properties(self):
+        for field in tic_settings._fields_:
+            if not field[0] == 'pin_settings':
+                prop = property(fget=partial(self._get_tic_settings_from_device, field[0]),
+                                fset=partial(self._set_tic_settings_with_option, field[0]))
+                setattr(self.__class__, field[0], prop)
+
+        for i in range(0, tc['TIC_CONTROL_PIN_COUNT']):
+            for field in pin_settings._fields_:
+                prop = property(fget=partial(self._get_pin_settings_from_device, field[0], i),
+                                fset=partial(self._set_pin_settings_with_option, field[0], i))
+                setattr(self.pin_settings[i].__class__, field[0], prop)
+
+    def _get_tic_settings_from_device(self, field, obj):
+        self._pull_device_settings()
+        return getattr(self._device_settings, field)
+
+    def _set_tic_settings_with_option(self, field, obj, value):
+        setattr(self._local_settings, field, value)
+        if (self.auto_apply):
+            self.apply()
+
+    def _get_pin_settings_from_device(self, field, pin_num, obj):
+        self._pull_device_settings()
+        return getattr(self._device_settings.pin_settings[pin_num], field)
+
+    def _set_pin_settings_with_option(self, field, pin_num, obj, value):
+        setattr(self._local_settings.pin_settings[pin_num], field)
+        if (self.auto_apply):
+            self.apply()
+
+        
+    @TED
+    def _pull_device_settings(self):
+        e_p = ticlib.tic_get_settings(byref(self._device_handle),
+                                      byref(self._device_settings_p))
+        self._device_settings = self._device_settings_p[0]
+        return e_p
+
+    @TED
+    def _set_settings(self):
+        e_p = ticlib.tic_set_settings(byref(self._device_handle),
+                                      byref(self._local_settings))
+        return e_p
+        
+    def _fill_with_defaults(self, product):
+        self._local_settings.product = product
+        ticlib.tic_settings_fill_with_defaults(byref(self._local_settings))
+
+    def apply(self):
         self._settings_fix()
-        self._set_settings(device_handle)
-        self._reinitialize(device_handle)
+        self._set_settings()
+        self._reinitialize()
 
     @TED
     def _settings_fix(self):
         warnings_p = POINTER(c_char_p)()
-        e_p = ticlib.tic_settings_fix(byref(self.settings),warnings_p)
+        e_p = ticlib.tic_settings_fix(byref(self._local_settings),warnings_p)
         if bool(warnings_p):
             for w in warnings_p:
-                logger.warning(w)
+                self._logger.warning(w)
         return e_p
 
     @TED
-    def _set_settings(self, device_handle):
-        e_p = ticlib.tic_set_settings( \
-            byref(device_handle), byref(self.settings))
+    def _reinitialize(self):
+        e_p = ticlib.tic_reinitialize(byref(self._device_handle))
         return e_p
 
-    @TED
-    def _settings_create(self):
-        settings_p = POINTER(tic_settings)()
-        e_p = ticlib.tic_settings_create(byref(settings_p))
-        self._settings = settings_p[0]
-        return e_p
-
-    @TED
-    def _reinitialize(self, device_handle):
-        e_p = ticlib.tic_reinitialize(byref(device_handle))
-        return e_p
-
-    def _load_config_settings(self):
-        with open(self._config_file, 'r') as ymlfile:
+    def load_config(self, config_file):
+        with open(config_file, 'r') as ymlfile:
             cfg = yaml.load(ymlfile)
 
-        cfg_settings = cfg['settings']
+        cfg_settings = cfg['tic_settings']
 
-        setting_names = []
-        setting_types = []
-        for setting in self._settings._fields_:
-            setting_names.append(setting[0])
-            setting_types.append(setting[1])
+        tic_settings_list = []
+        for setting in tic_settings._fields_:
+            tic_settings_list.append(setting[0])
 
         for setting in cfg_settings: 
-            if setting in setting_names:
+            if setting in tic_settings_list:
                 if setting == 'pin_settings':
-                    for pin in cfg_settings[setting]:
-                            i = t_const[pin['pin_num']]
-                            if 'func' in pin:
-                                self.settings.pin_settings[i].func = t_const[pin['func']]
-                            if 'pullup' in pin:
-                                self.settings.pin_settings[i].pullup = pin['pullup']
-                            if 'analog' in pin:
-                                self.settings.pin_settings[i].analog = pin['analog']
-                            if 'polarity' in pin:
-                                self.settings.pin_settings[i].polarity = pin['polarity']
+                    for pin in cfg_settings['pin_settings']:
+                        i = tc[pin['pin_num']]
+                        if 'func' in pin:
+                            self._local_settings.pin_settings[i].func = tc[pin['func']]
+                        if 'pullup' in pin:
+                            self._local_settings.pin_settings[i].pullup = pin['pullup']
+                        if 'analog' in pin:
+                            self._local_settings.pin_settings[i].analog = pin['analog']
+                        if 'polarity' in pin:
+                            self._local_settings.pin_settings[i].polarity = pin['polarity']
                 else:
-                    value = cfg_settings[setting]
-                    idx = setting_names.index(setting)
-                    if 'TIC' in str(value):
-                        c_val = setting_types[idx](t_const[value]) 
+                    if 'TIC' in str(cfg_settings[setting]):
+                        value = tc[cfg_settings[setting]]
                     else:
-                        c_val = setting_types[idx](value)
-                    setattr(self.settings, setting, c_val)
+                        value = cfg_settings[setting]
+                    setattr(self._local_settings, setting, value)
+
+        if (self.auto_apply):
+            self.apply()
 
 if __name__ == '__main__':
 
     tic = PyTic()
     tic.connect_to_serial_number('00219838')
+    tic.settings.load_config('config.yml')
+    #tic.setting.apply()
     tic.energize()
     tic.exit_safe_start()
     for i in range(0,5):
@@ -258,6 +327,24 @@ if __name__ == '__main__':
         sleep(1.5)
     tic.enter_safe_start()    
     tic.deenergize()
+    tic.variables.error_status
+ 
+ 
+    # print(tic.settings.product)
+    # print(tic.settings.serial_response_delay)
+    # print(tic.settings.current_limit)
+    # for i in range(0, 5):
+    #     print(tic.settings.pin_settings[i].pullup)
+
+    # tic.energize()
+    # tic.exit_safe_start()
+    # for i in range(0,5):
+    #     tic.set_target_position(4000)
+    #     sleep(1.5)
+    #     tic.set_target_position(0)
+    #     sleep(1.5)
+    # tic.enter_safe_start()    
+    # tic.deenergize()
     # var = PyTic_Variable(tic.handle)
     # sleep(2)
     # print(var.target_position)
@@ -312,11 +399,11 @@ if __name__ == '__main__':
     #     product_int = ticlib.tic_settings_get_product(byref(self._settings))
     #     chklist = ["TIC_PRODUCT_T825", "TIC_PRODUCT_T834", "TIC_PRODUCT_T500"]
     #     for p in chklist:
-    #         if product_int == t_const[p]:
+    #         if product_int == tc[p]:
     #             return p;
     # @product.setter
     # def product(self, product_str):
-    #     ticlib.tic_settings_set_product(byref(self._settings),c_uint8(t_const[product_str]))
+    #     ticlib.tic_settings_set_product(byref(self._settings),c_uint8(tc[product_str]))
 
     # @property
     # def auto_clear_driver_error(self):
