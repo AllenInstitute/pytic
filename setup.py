03ddc53f1abef04a3bbd94fcea1b335c7e1d2668
ALLENINST\danc <danc@alleninstitute.org>
2018-07-24 14:28:51 -0700

    v0.0.2 Release, msi added to install script

--- setup.py
+++ setup.py
@@ -1,9 +1,25 @@
 __author__ = "Daniel Castelli"
 from setuptools import setup
+from setuptools.command.install import install as _install
+import subprocess
+
+class install(_install):
+    def pre_install_script(self):
+        pass
+
+    def post_install_script(self):
+        subprocess.call("msiexec /i pytic\\drivers\\pololu-tic-1.6.2-win.msi")
+
+    def run(self):
+        self.pre_install_script()
+
+        _install.run(self)
+
+        self.post_install_script()
 
 setup(
     name = "PyTic",
-    version = "0.0.1",
+    version = "0.0.2",
     author = "Daniel Castelli",
     #email = "",
     description = "An object-oriented Python wrapper for Pololu Tic Stepper Controllers.",
@@ -13,4 +29,5 @@ setup(
     packages = ['pytic'],
     install_requires = ['PyYAML'],
     include_package_data=True,
+    cmdclass = {'install': install},
 )
